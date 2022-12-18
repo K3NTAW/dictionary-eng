a011ffbdea3621919c844f8f4e22d52fc2859cd9
Kenta Laptop <kenta.waibel@gmail.com>
2022-12-18 20:15:23 +0100

    feature: add more words to the dictionary
    by Kenta Waibel

--- src/main/java/dictionary.java
+++ src/main/java/dictionary.java
@@ -1,29 +1,49 @@
+import java.util.*;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.List;
 import java.util.Set;
-
-
 
 public class dictionary {
     public static void main(String[] args) {
+        boolean bool = true;
+        Random rand = new Random();
+        Scanner keyboard = new Scanner(System.in);
         wordbook wordbook = new wordbook();
         HashMap<String, String> data = wordbook.gethishashmap();
-
-
         Set<String> keys = data.keySet();
+        List<String> keyList = new ArrayList<>(keys);
 
-        Scanner keyboard = new Scanner(System.in);
-        boolean bool = true;
+
 
 
         while (bool) {
+            boolean ranbool = true;
             System.out.println("");
-            System.out.println("Do you wanna search a word or list all the words in the dictionary? list/search");
+            System.out.println("What do you want to do? search,list or get a random word?");
+            System.out.println("write (stop) to finish");
             String word = keyboard.nextLine();
             if (Objects.equals(word, "list")) {
-                for (String key : keys) {
+                Collections.sort(keyList);
+                for (String key : keyList) {
                     System.out.println(key);
+                }
+            } else if (Objects.equals(word,"random")) {
+                while (ranbool) {
+                    Collections.shuffle(keyList);
+                    String randomKey = keyList.get(0);
+                    String randomValue = data.get(randomKey);
+                    System.out.println("Your Word:" + randomKey);
+                    System.out.println("Do you want the description? yes/no");
+                    String answer = keyboard.nextLine();
+                    if (Objects.equals(answer, "yes")) {
+                        String description = data.get(randomKey);
+                        System.out.println("Description: " + description);
+                        ranbool = false;
+                    } else {
+                    System.out.println("okayy, have fun searching!!");
+                    ranbool = false;
+                    }
                 }
             } else if (Objects.equals(word, "search")) {
                 System.out.println("Wich word do you wanna search?");
@@ -41,18 +61,5 @@ public class dictionary {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
